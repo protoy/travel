d8eef9926e6cdfec0112d32571c961cbe059d37c
protoy <37529943+protoy@users.noreply.github.com>
2020-04-03 21:01:12 +0800

    'city-router'

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Home from '../pages/home/Home.vue'
+import City from '../pages/city/City.vue'
 
 Vue.use(VueRouter)
 
@@ -10,15 +11,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  }, {
+    path: '/city',
+    name: 'City',
+    component: City
   }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
