01d94f67c94d984122b820745a05f09968bede4e
protoy <37529943+protoy@users.noreply.github.com>
2020-04-19 23:43:36 +0800

    detail-banner

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ module.exports = {
         proxy: {
             '/api': {
                 target: 'http://localhost:8080',
-                target: 'http://localhost:8081',
                 // changeOrigin: true,
                 pathRewrite: {
                     '^/api': '/mock'
